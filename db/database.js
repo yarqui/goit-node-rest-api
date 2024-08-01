f5a64973ec88c88367ac9fb98e0d8ef520ba7164
yarqui <y.pelykh@gmail.com>
2024-08-01 12:37:11 -0400

    chore: edit sequelize initialization config, edit env

--- db/database.js
+++ db/database.js
@@ -1,18 +1,25 @@
 import { Sequelize } from "sequelize";
 
-const { HOST, USER, PASSWORD, DB } = process.env;
+const {
+  DATABASE_DIALECT,
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+} = process.env;
 
-const sequelize = new Sequelize(
-  `postgresql://${USER}:${PASSWORD}@${HOST}/${DB}`,
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
+const sequelize = new Sequelize({
+  dialect: DATABASE_DIALECT,
+  database: DATABASE_NAME,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 const connectDB = async () => {
   try {
