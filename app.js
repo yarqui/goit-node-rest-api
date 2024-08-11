208ea096737eacc74bd2f95fc79d05df74e17245
yarqui <y.pelykh@gmail.com>
2024-08-11 12:45:34 -0400

    add auth router to app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 const { SERVER_PORT = 3000 } = process.env;
 import { connectDB } from "./db/database.js";
 import contactsRouter from "./routes/contactsRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/auth", authRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
